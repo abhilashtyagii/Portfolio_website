8961c6f9cdcdff41a6f4e42c15f8ec70b9888a12
ABHILASH TYAGI <131495649+abhilashtyagii@users.noreply.github.com>
2023-10-02 14:58:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed= new Typed(".text", {
+    strings:["Web Developer." , "Software Engineer.","Frontend Developer."],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true 
+});
+
